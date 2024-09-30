d562000b8eff9883238a8a3aa30e51abb06042c1
ralf <capinding151515@gmail.com>
2024-09-30 17:59:23 +0800

    subtractionnn proccesss itoo

--- calculator.java
+++ calculator.java
@@ -1,5 +1,12 @@
 public class calculator{
     public static void(String[] args){
-        
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+        int c = a - b;
+
+
+        System.out.println("addition ito: " + c);
+        System.out.println("subtractionn ito: " + c);
     }
 }
