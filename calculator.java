03ba6587276188a3098044e08f4cb0a30a47fbee
ralf <capinding151515@gmail.com>
2024-09-30 17:56:22 +0800

    addtion proccesss itoo

--- calculator.java
+++ calculator.java
@@ -1,5 +1,10 @@
 public class calculator{
     public static void(String[] args){
-        
+        int a = 5;
+        int b = 10;
+        int c = a + b;
+
+
+        System.out.println("addition ito: " + c)
     }
 }
